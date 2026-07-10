cdb4d2d2a7d0fce9b92399cb290c32f8f29a2319
Tushar <tt698887@gmail.com>
2026-07-10 10:52:37 +0530

    Add files via upload

---
+++ drumindex.js
@@ -0,0 +1,35 @@
+document.querySelector(".w").addEventListener("click", function () {
+    console.log("W");
+    new Audio("sounds/crash.mp3").play();
+});
+    
+document.querySelector(".a").addEventListener("click", function () {
+    console.log("A");
+    new Audio("sounds/kick.mp3").play();
+});
+
+document.querySelector(".s").addEventListener("click", function () {
+    console.log("S");
+    new Audio("sounds/snare.mp3").play();
+});
+
+document.querySelector(".d").addEventListener("click", function () {
+    console.log("D");
+    new Audio("sounds/tom-1.mp3").play();
+});
+
+document.querySelector(".j").addEventListener("click", function () {
+    console.log("J");
+    new Audio("sounds/tom-2.mp3").play();
+});
+
+document.querySelector(".k").addEventListener("click", function () {
+    console.log("K");
+    new Audio("sounds/tom-3.mp3").play();
+});
+
+document.querySelector(".l").addEventListener("click", function () {
+    console.log("L");
+    new Audio("sounds/tom-4.mp3").play();
+});
+
